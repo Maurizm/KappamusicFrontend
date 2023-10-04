08444b98f1b42dda83bf3bbcad106565a64c22e1
Maurizm <89363295+Maurizm@users.noreply.github.com>
2023-10-04 18:37:54 -0400

    pagina busqueda

---
+++ src/pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,5 @@
+import React, {useEffect} from "react"
+import "./styles.css";
+import MusicPlayer from "../components/MusicPlayer";
+import { COLORS } from "../colors/colors";
+import { useState } from "react";
