572a10bee0be37a4257ceb2b1d611ad37e3d4138
William <wimoca95@gmail.com>
2023-10-27 11:11:32 -0400

    Bug v6

--- src/firebase/hooks/removeFavorites.js
+++ src/firebase/hooks/removeFavorites.js
@@ -12,11 +12,13 @@ import {
 import { auth, db } from "../credenciales";
 
 export const removeFavorites = async (song) => {
-  const q = query(
-    collection(db, "users"),
-    where("email", "==", auth.currentUser.email)
-  );
-  await getDocs(q).then((querySnapshot) => {
+  // const q = query(
+  //   collection(db, "users"),
+  //   where("email", "==", auth.currentUser.email)
+  // );
+  await getDocs(
+    query(collection(db, "users"), where("email", "==", auth.currentUser.email))
+  ).then((querySnapshot) => {
     querySnapshot.docs.map((doc) => {
       updateDoc(doc.ref, {
         ...doc.data(),
--- src/firebase/hooks/updateFavorites.js
+++ src/firebase/hooks/updateFavorites.js
@@ -11,11 +11,13 @@ import {
 import { auth, db } from "../credenciales";
 
 export const updateFavorites = async (song) => {
-  const q = query(
-    collection(db, "users"),
-    where("email", "==", auth.currentUser.email)
-  );
-  await getDocs(q).then((querySnapshot) => {
+  // const q = query(
+  //   collection(db, "users"),
+  //   where("email", "==", auth.currentUser.email)
+  // );
+  await getDocs(
+    query(collection(db, "users"), where("email", "==", auth.currentUser.email))
+  ).then((querySnapshot) => {
     querySnapshot.docs.map((doc) => {
       updateDoc(doc.ref, {
         ...doc.data(),
